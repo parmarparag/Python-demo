70e2e57075ccc47df3e14e2cd3ac8f3c462b0da0
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-06-21 20:09:31 +0530

    variable
    python variables

---
+++ variable.py
@@ -0,0 +1,16 @@
+a="parag"    #string data type
+b=15         #int data type
+c=20.5       #float data type
+d=True       #boolean data type
+
+#printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+#printing the types of  variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
