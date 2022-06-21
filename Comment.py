f237765071aa860d653f844f6b54364b8e84d846
Parmar Parag <107787295+parmarparag@users.noreply.github.com>
2022-06-21 20:08:48 +0530

    comment
    python comments

---
+++ Comment.py
@@ -0,0 +1,14 @@
+#this is a single line comment
+#print("Hello World")
+
+'''
+this is 
+multi-line
+ comment 
+'''
+
+'''
+this is example of multi-line comment
+print("Hello python")
+print("python is a free and easy language")
+'''
